798e59754dc10781343400fd759b584e812e2c65
naitikshaxma <naitik.sharma_cs.aiml24@gla.ac.in>
2026-02-19 08:49:08 +0530

    THIRD COMMIT

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 /**
  * @file main.ino
  * @brief Embedded Touch Detection System using TTP223
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author NAITIK SHARMA
+ * @date 2026-02-19
  *
  * @details
  * Reads digital touch input from TTP223 sensor
